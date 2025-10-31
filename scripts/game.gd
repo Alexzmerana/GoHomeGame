0a9522d3b7e3248e2da8614f5c9c4b386c6ec930
Alex Merana <azmerana03@gmail.com>
2025-10-31 11:09:42 -0400

    added cannon sprite + launch angle

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,40 +1,58 @@
 extends Node2D
 
+@onready var ground := $Ground
+@onready var player := $Player
+@onready var camera := $Camera2D
+@onready var cannon := $Cannon
+
 const GROUND_PIXEL_WIDTH := 2304
+
 @warning_ignore("integer_division")
-const GROUND_RESET_POSITION = GROUND_PIXEL_WIDTH / 2
+const GROUND_START_POSITION = Vector2(1152.0, 40.0)
+
 
 # The starting position of the sprite is at 1152. When the sprite is at postion 0 we are half way 
 # through and it needs to be reset to maintain illusion of movement
 const GROUND_RESET_POSITION_THRESHHOLD = 0
 
+const CANNON_START_POSITION := Vector2(-22, 0)
+
+const PLAYER_START_POITION := Vector2(24, 0)
 const MAX_SPEED := 2000
-const MAX_ZOOM := Vector2(3.0, 3.0)
-const MIN_ZOOM := Vector2(2.5, 2.5)
+const MAX_ZOOM := Vector2(2.5, 2.5)
+const MIN_ZOOM := Vector2(2.0, 2.0)
 
 @export var speed : float = 0.0
+@export var launched := false
 
-@onready var ground := $Ground
-@onready var camera := $Player/Camera2D
+
 
 var screen_size : Vector2i
 
 func _ready() -> void:
+	launched = false
 	screen_size = get_window().size
-	print(screen_size)
 	print(ground.position)
+	print(player.position)
 	
 	
 func new_launch() -> void:
-	ground.position.x = GROUND_RESET_POSITION
+	launched = false
+	player.visible = false
+	ground.position = GROUND_START_POSITION
+	cannon.position = CANNON_START_POSITION
+	player.position = PLAYER_START_POITION
+
 
 func _process(delta: float) -> void:
+	print(player.position)
 	ground.position.x -= speed * delta
+	cannon.position.x -= speed * delta
 	if ground.position.x < GROUND_RESET_POSITION_THRESHHOLD:
 		@warning_ignore("integer_division")
-		ground.position.x = GROUND_RESET_POSITION
+		ground.position = GROUND_START_POSITION
 		
-	
+		
 	var zoom_amount := remap(speed, 0.0, MAX_SPEED, 0.0, 1.0)
 	var target_zoom := MAX_ZOOM.lerp(MIN_ZOOM, zoom_amount)
 	camera.zoom = camera.zoom.lerp(target_zoom, delta * 5.0)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
 @onready var game_node = $".."
-
+@onready var cannon_shaft = $"../Cannon/cannon_shaft"
+@onready var cannon = $"../Cannon"
 
 const SPEED := 1 
 const JUMP_VELOCITY := -400.0
 const GRAVITY := 30.0
 const MAX_Y_POSITION := 0
 const MIN_SPEED := .01
-var launched := false
-var launch_velocity := Vector2(100, -10)
+
+var launch_velocity := Vector2(100, -5)
 var player_velocity := Vector2(0,0)
 var friction_scalar := 0.5
+var account_balance := 0
+
+func _ready() -> void:
+	visible = false
 
 func check_and_apply_bounce() -> void:
 	if position.y > MAX_Y_POSITION: 
@@ -21,15 +26,24 @@ func check_and_apply_bounce() -> void:
 		if player_velocity.x <= MIN_SPEED:
 			player_velocity *= 0
 
+func get_launch_angle() -> Vector2:
+	var direction_vector = Vector2(cos(cannon_shaft.rotation), sin(cannon_shaft.rotation)).normalized()
+	return direction_vector
+	
 func _physics_process(delta: float) -> void:
-	position.y += player_velocity.y * delta
-	check_and_apply_bounce()
-	player_velocity.y += GRAVITY * delta
-	game_node.speed = player_velocity.x
+	if not game_node.launched:
+			position = get_launch_angle()
+	else:
+		position.y += player_velocity.y * delta
+		check_and_apply_bounce()
+		player_velocity.y += GRAVITY * delta
+		game_node.speed = player_velocity.x
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action("launch_action") and not launched:
-		player_velocity += launch_velocity * 10
-		game_node.speed = player_velocity.x
-		
-		launched = true  
+	if event.is_action("launch_action") and not game_node.launched:
+		var launch_angle := get_launch_angle()
+		position = cannon_shaft.position + launch_angle * cannon.cannon_length
+		visible = true
+		player_velocity = launch_angle * cannon.launch_power
+		game_node.speed = player_velocity.x 
+		game_node.launched = true  
