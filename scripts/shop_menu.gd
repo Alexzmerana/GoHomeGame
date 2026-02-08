4aaf22c4a9bd92bc2689d3d24a68751981752112
Alex <azmerana03@gmail.com>
2026-02-08 15:24:04 -0500

    Shop menu

--- scripts/shop_menu.gd
+++ scripts/shop_menu.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
-
+$"."
+var shop_hash := {
+	"upgrades" : [
+		{"name" : "Cannon Power v1", "cost" : 500},
+		{"name" : "Cannon Power v2", "cost" : 1000},
+		{"name" : "Cannon Power v3", "cost" : 1500}
+	]
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
